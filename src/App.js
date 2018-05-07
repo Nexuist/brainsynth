3310ea5cb5c2d17c2001cf445280109829a390af
Andi Andreas <nexuister@gmail.com>
2018-05-07 01:19:48 -0400

    Build simple navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">
+            Brainsynth
+          </a>
+          <span className="navbar-text">Demo Version</span>
+        </nav>
+        <nav className="navbar navbar-dark bg-dark" style={{"justify-content": "flex-start"}}>
+          <span className="navbar-text">Your Song Name</span>
+          <button class="btn btn-info">
+            <i class="fas fa-pencil-alt fa-inverse" />
+          </button>
+          <button class="btn btn-success">
+            <i class="fas fa-play" />
+          </button>
+        </nav>
       </div>
     );
   }
